e8ce5ceee53074b448ac0b3750548628e5a234c0
chandramavuli <chandrusmart1218@gmail.com>
2022-04-07 12:18:02 +0000

    added user-home

--- angularapp/src/app/user-home/user-home.component.ts
+++ angularapp/src/app/user-home/user-home.component.ts
@@ -1,4 +1,9 @@
+import { UserAuthService } from './../services/user-auth.service';
+import { AddIssue } from './../model/add-issue';
+import { RegisterIssueService } from './../services/register-issue.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../model/user';
 
 @Component({
   selector: 'app-user-home',
@@ -6,10 +11,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-home.component.css']
 })
 export class UserHomeComponent implements OnInit {
-
-  constructor() { }
-
+   issueList!:AddIssue[];
+   user = new User();
+   userIssue!:AddIssue[];
+  userDetail!:'';
+  constructor(private service:RegisterIssueService,private userAuthService:UserAuthService, private router: Router) { }
+  count!:Number;
   ngOnInit(): void {
+  
+   
+    
+     
+    this.userDetail=this.userAuthService.getUser();
+    this.service.getIssueList(this.userAuthService.getUser()).subscribe(//
+    data=>{
+      this.userIssue = data;
+      console.log("Response recieved")
+      console.log(data)
+    },
+    error=>console.log("Exception occured")
+  )
   }
+   public logout(){
+    this.user.active=false;
+    this.router.navigate(['/login'])
+   return this.userAuthService.clear();
+      
+  }
+
+  
 
 }
